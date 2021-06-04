d50166b0dc857cafd357f7c9268e10d1117d26ab
Fezz <43589668+FBFezz@users.noreply.github.com>
2021-06-04 14:13:28 +0100

    Added Blip and Cooldown

--- client/client.lua
+++ client/client.lua
@@ -1,9 +1,11 @@
-ESX = nil
-
 local hasStarted, startedHacking, cancontinue = false, false, false
 local CurrentCoords, started = nil, nil
 local phoneModel = Config.PhoneModel
 local taken = 0
+local pos = GetEntityCoords(GetPlayerPed(-1),  true)
+local s1, s2 = GetStreetNameAtCoord( pos.x, pos.y, pos.z, Citizen.PointerValueInt(), Citizen.PointerValueInt() )
+local street1 = GetStreetNameFromHashKey(s1)
+local street2 = GetStreetNameFromHashKey(s2)
 local PlayerPed = PlayerPedId()
 
 DeleteObject(prop)
@@ -16,50 +18,51 @@ end)
 
 AddEventHandler('onClientResourceStart', function (resourceName)
 	if (GetCurrentResourceName() ~= resourceName) then return end
-	for k,v in pairs(Config.AtmModels) do 
-		if Config.FivemTarget then
-		    exports["fivem-target"]:AddTargetModel({
-		    	name = "robbery",
-		    	label = "ATM Robbery",
-		    	icon = "fas fa-dog",
-		    	model = GetHashKey(v.prop),
-		    	interactDist = 2.0,
-		    	onInteract = StartHacking,
-		    	options = {
-		    	  {
-		    		name = "rob",
-		    		label = "Rob ATM"
-		    	  }
-		    	},
+	    for k,v in pairs(Config.AtmModels) do 
+		    if Config.FivemTarget then
+		        exports["fivem-target"]:AddTargetModel({
+		    	    name = "robbery",
+		    	    label = "ATM Robbery",
+		    	    icon = "fas fa-dog",
+		    	    model = GetHashKey(v.prop),
+		    	    interactDist = 2.0,
+		    	    onInteract = StartHacking,
+		    	    options = {
+		    	        {
+		    		        name = "rob",
+		    		        label = "Rob ATM"
+		    	        }
+		    	    },
 		    	vars = {}
-	 	    })
-		else
-			local atms = {
-				-870868698,
-				-1126237515,
-				-1364697528,
-				506770882,
-			}
-			exports["bt-target"]:AddTargetModel(atms, {
-				options = {
-					{
-						event = "szi_atmrobbery:startHacker",
-						icon = "fas fa-piggy-bank",
-						label = "Rob ATM",
-					},
-				},
-				job = {"all"},
-				distance = 4.5
-			})
-		end
-	end    
+	 	        })
+		    else
+			    local atms = {
+			    	-870868698,
+			    	-1126237515,
+			    	-1364697528,
+			    	506770882,
+			    }
+			    exports["bt-target"]:AddTargetModel(atms, {
+			    	options = {
+			    		{
+			    			event = "szi_atmrobbery:startHacker",
+			    			icon = "fas fa-piggy-bank",
+			    			label = "Rob ATM",
+			    		},
+			    	},
+			    	job = {"all"},
+			    	distance = 4.5
+			    })
+		    end
+	    end    
 end)
 
 function FinishHackings(success)
+	DeleteObject(prop)
 	TriggerServerEvent("szi_atmrobbery:hackSuccess",success)
 	FinishHacking(success)
+	Cooldown(true)
 	startedHacking = false
-	DeleteObject(prop)
 end
 
 function FinishHacking(success)
@@ -104,7 +107,7 @@ function FinishHacking(success)
 		ClearPedTasks(PlayerPed)
 		cancontinue = false
 		taken = 0
-		Wait(2000)
+		Citizen.Wait(Config.CooldownTime * 1000)
 		hasStarted = false
 	end
 end
@@ -126,11 +129,12 @@ function StartHacking()
 			    newPhoneProp()
 			    RequestAnimDict(Config.HackingDict)
 			    while not HasAnimDictLoaded(Config.HackingDict) do
-			      Wait(1)
+			        Wait(1)
 			    end
 			    TaskPlayAnim(PlayerPed,Config.HackingDict,Config.HackingAnim ,8.0,8.0,-1,1,0,false,false,false)
 			    TriggerEvent("mhacking:show")
 			    TriggerEvent("mhacking:start",5,30,FinishHackings)
+				TriggerServerEvent('szi_atmrobbery:notifyPolice', street1, street2, pos)
 		    else
 			    ESX.ShowHelpNotification(_U('cant_hack'), false, true, 2000)
 			    Wait(2000)
@@ -145,3 +149,42 @@ RegisterNetEvent("szi_atmrobbery:startHacker")
 AddEventHandler("szi_atmrobbery:startHacker", function()
 	StartHacking()
 end)
+
+RegisterNetEvent('szi_atmrobbery:notifyPolice')
+AddEventHandler('szi_atmrobbery:notifyPolice', function(msg)
+    exports['mythic_notify']:DoHudText('error', msg)
+end)
+
+RegisterNetEvent('szi_atmrobbery:blip')
+AddEventHandler('szi_atmrobbery:blip', function(x,y,z)
+  Blip = AddBlipForCoord(x,y,z)
+    SetBlipSprite(Blip,  500)
+    SetBlipColour(Blip,  1)
+    SetBlipAlpha(Blip,  250)
+    SetBlipDisplay(Blip, 4)
+    SetBlipScale(Blip, 1.2)
+    SetBlipFlashes(Blip, true)
+    SetBlipAsShortRange(Blip,  true)
+    BeginTextCommandSetBlipName('STRING')
+    AddTextComponentString('Robbery In Progress | ATM')
+    EndTextCommandSetBlipName(Blip)
+    Wait(Config.BlipTimer * 1000)
+    RemoveBlip(Blip)
+end)
+
+function Cooldown(hasStarted)
+    local timer = Config.CooldownTime
+    while hasStarted == true do
+        Citizen.Wait(1000)
+
+        if timer > 0 then
+            timer = timer -1
+        end
+
+        if timer == 1 then
+			hasStarted = false
+			startedHacking = false
+            break
+        end
+    end
+end
