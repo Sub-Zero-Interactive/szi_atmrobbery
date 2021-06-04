9aeebceda1f6d90983b66a85986ef3119e134ae9
Fezz <43589668+FBFezz@users.noreply.github.com>
2021-06-04 14:20:23 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'Sub-Zero Interactive'
 description 'This is a script that allows you to rob atms using fivem-target or bt-target.'
-version '1.1'
+version '1.2'
 
 server_scripts {
     '@es_extended/locale.lua',
