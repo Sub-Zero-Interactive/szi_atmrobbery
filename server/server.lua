ba5f3a6d026a5dffabc4f79d38682fa8c4e8d156
Fezz <43589668+FBFezz@users.noreply.github.com>
2021-06-19 01:22:43 +0100

    Added version checker

--- server/server.lua
+++ server/server.lua
@@ -77,3 +77,48 @@ AddEventHandler('szi_atmrobbery:notifyPolice', function(street1, street2, pos)
 		end
 	end
 end)
+
+Citizen.CreateThread(function()
+        local vRaw = LoadResourceFile(GetCurrentResourceName(), 'version.json')
+        if vRaw then
+            local v = json.decode(vRaw)
+            PerformHttpRequest('https://raw.githubusercontent.com/Sub-Zero-Interactive/szi_atmrobbery/main/version.json', function(code, res, headers)
+                if code == 200 then
+                    local rv = json.decode(res)
+                    if rv.version == v.version then
+                        if rv.commit ~= v.commit then 
+                            print(([[
+^1----------------------------------------------------------------------
+^1WARNING: YOUR SZI_ATMROBBERY IS OUTDATED!
+^1COMMIT UPDATE: ^5%s AVAILABLE
+^1DOWNLOAD:^5 https://github.com/Sub-Zero-Interactive/szi_atmrobbery
+^1CHANGELOG:^5 %s
+^1-----------------------------------------------------------------------
+^0]]):format(rv.commit, rv.changelog))
+                        else
+                            print(([[
+^8-------------------------------------------------------
+^2Your szi_atmrobbery is the latest version!
+^5Version:^0 %s
+^5COMMIT:^0 %s
+^5CHANGELOG:^0 %s
+^8-------------------------------------------------------
+^0]]):format( rv.version, rv.commit, rv.changelog))
+                        end
+                    else
+                        print(([[
+^1----------------------------------------------------------------------
+^1URGENT: YOUR SZI_ATMROBBERY IS OUTDATATED!!!
+^1COMMIT UPDATE: ^5%s AVAILABLE
+^1DOWNLOAD:^5 https://github.com/Sub-Zero-Interactive/szi_atmrobbery
+^1CHANGELOG:^5 %s
+^1-----------------------------------------------------------------------
+^0]]):format(rv.commit, rv.changelog))
+                    end
+                else
+                    print('[^1ERROR^0] szi_atmrobbery unable to check version!')
+                end
+            end,'GET'
+        )
+    end
+end)
